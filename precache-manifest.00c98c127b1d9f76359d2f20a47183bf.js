f6865712b709c7e15e1d1c176450ce9b78363bff
sanjaypj <sanjaypjayan2000@gmail.com>
2019-10-13 10:13:03 -0400

    Updates

---
+++ precache-manifest.00c98c127b1d9f76359d2f20a47183bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc975a6ed2ca59964ad32fcaf3cc822",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "7c254acc7cda9d75ab81",
+    "url": "/react-todo/static/css/main.6f733fa3.chunk.css"
+  },
+  {
+    "revision": "1aec9853fb6c2a18aa64",
+    "url": "/react-todo/static/js/2.6316e7aa.chunk.js"
+  },
+  {
+    "revision": "7c254acc7cda9d75ab81",
+    "url": "/react-todo/static/js/main.265ed08e.chunk.js"
+  },
+  {
+    "revision": "bcb9417c59d3104e998e",
+    "url": "/react-todo/static/js/runtime-main.62899147.js"
+  }
+]);
